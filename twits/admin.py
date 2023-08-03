99784379d02812ef3e3572bd22cddf382b248ef4
Schyler Lowry <schyler_lowry@outlook.com>
2023-08-03 19:43:24 -0400

    Add custom Model Admin to admin.py

--- twits/admin.py
+++ twits/admin.py
@@ -8,5 +8,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Twit, Comment
 
-admin.site.register(Twit)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    
+
+
+class TwitAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    fields = ['body', 'image_url','author', 'likes', 'created', 'updated',]
+    readonly_fields = ('created', 'updated',)
+
+admin.site.register(Twit, TwitAdmin)
 admin.site.register(Comment)
